5a226a961931bbbcc87a5cac4e8c623fbff981eb
Wedika Jadhav <vedikajadhav48@gmail.com>
2015-03-30 03:43:51 -0700

    Complete

--- app/src/main/java/com/example/vedikajadhav/cardgame/ImageAdapter.java
+++ app/src/main/java/com/example/vedikajadhav/cardgame/ImageAdapter.java
@@ -2,16 +2,14 @@ package com.example.vedikajadhav.cardgame;
 
 import android.app.Activity;
 import android.content.Context;
-import android.media.Image;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -19,21 +17,24 @@ import java.util.Random;
  * Created by Vedika Jadhav on 3/24/2015.
  */
 public class ImageAdapter extends ArrayAdapter<BlankImageWithFaceImage>{
+    private static final String TAG = "ImageAdapter";
     private Context mContext;
+    private ArrayList<BlankImageWithFaceImage> cardArray;
 
     public ImageAdapter(ArrayList<BlankImageWithFaceImage> mCardArrayList, Context context) {
         super(context.getApplicationContext(), 0, mCardArrayList);
-       // cardArray = mCardArrayList;
+        cardArray = mCardArrayList;
         mContext = context;
+        Log.i(TAG, "size" +cardArray.size());
         //used to show up random images in gridView
         Random rnd = new Random();
-/*        for (int i = imageIds.length - 1; i >= 0; i--) {
+        for (int i =  cardArray.size()- 1; i >= 0; i--) {
             int index = rnd.nextInt(i + 1);
             // Simple swap
-            blankImageWithFaceImage temp = imageIds[index];
-            imageIds[index] = imageIds[i];
-            imageIds[i] = temp;
-        }*/
+            BlankImageWithFaceImage temp = cardArray.get(index);
+            cardArray.set(index, cardArray.get(i));
+            cardArray.set(i, temp);
+        }
     }
 
     @Override
@@ -45,45 +46,16 @@ public class ImageAdapter extends ArrayAdapter<BlankImageWithFaceImage>{
         //Configure the view for this Card(BlankImageWithFaceImage)
         BlankImageWithFaceImage blankImageWithFaceImage = getItem(position);
         ImageView blankImage = (ImageView)convertView.findViewById(R.id.grid_item_blank_image);
-        blankImage.setImageResource(blankImageWithFaceImage.getBlankImageId());
-        blankImage.setBackgroundResource(blankImageWithFaceImage.getFaceImageId());
+        Resources r = mContext.getResources();
+        Drawable[] layers = new Drawable[2];
+        layers[0] = r.getDrawable(blankImageWithFaceImage.getFaceImageId());
+        layers[1] = r.getDrawable(blankImageWithFaceImage.getBlankImageId());
+        LayerDrawable layerDrawable = new LayerDrawable(layers);
+        blankImage.setImageDrawable(layerDrawable);
+
+
+        /*blankImage.setImageResource(blankImageWithFaceImage.getBlankImageId());
+        blankImage.setBackgroundResource(blankImageWithFaceImage.getFaceImageId());*/
         return convertView;
     }
-
-/*    public void swapImageIds(int position) {
-        if (cardStatus[position] == false) {
-            Log.i("MYLOG", position + "");
-            int bId = imageIds[position].getBlankImageId();
-            imageIds[position].setBlankImageId(imageIds[position]
-                    .getFaceImageId());
-            imageIds[position].setFaceImageId(bId);
-            cardArray.add(position);
-
-            if (cardArray.size() == 1) {
-                return;
-            }
-
-            if (cardArray.size() == 2) {
-                int firstImageId = imageIds[cardArray.get(0)].getBlankImageId();
-                int secondImageId = imageIds[cardArray.get(1)].getBlankImageId();
-
-                if (firstImageId == secondImageId) {
-                    cardStatus[cardArray.get(0)] = true;
-                    cardStatus[cardArray.get(1)] = true;
-                    cardsMatched++;
-
-                    cardArray.clear();
-                    return;
-                }
-            }
-
-            for (int i = 0; i < cardArray.size(); i++) {
-                restoreImageIds(cardArray.get(i));//if the cards dont match flip them back
-            }
-            cardArray.clear();
-        } else {
-           // MainActivity.flipCount--;
-        }
-        return;
-    }*/
 }
--- app/src/main/java/com/example/vedikajadhav/cardgame/MainActivity.java
+++ app/src/main/java/com/example/vedikajadhav/cardgame/MainActivity.java
@@ -1,30 +1,22 @@
 package com.example.vedikajadhav.cardgame;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
-import java.util.Date;
-
 
 public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
-
-    public static int flipCount = 0;
+    private static final String TAG = "MainActivity";
+    private static int flipCount = 0;
     private ImageAdapter imageAdapter;
     private Integer[] faceCardList = {R.drawable.club2, R.drawable.club8, R.drawable.diamond6, R.drawable.diamond9, R.drawable.diamond_king,
             R.drawable.heart_king, R.drawable.heart_jack, R.drawable.heart_queen, R.drawable.heart3, R.drawable.spade4, R.drawable.spade_king,
@@ -41,110 +33,39 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         flipCountEditText = (EditText) findViewById(R.id.flipCountEditText);
         flipCountEditText.setText("" +flipCount);
 
         GridView grid = (GridView) findViewById(R.id.gridView1);
-        createCardArrayList(cardArrayList);
+        /*createCardArrayList(cardArrayList);*/
         imageAdapter = new ImageAdapter(cardArrayList, this);
+
+        Object object = getLastCustomNonConfigurationInstance();
+        if(null != object){
+            imageAdapter = (ImageAdapter)object;
+        }
+        else{
+            createCardArrayList(cardArrayList);
+        }
         grid.setAdapter(imageAdapter);
         grid.setOnItemClickListener(this);
+    }
 
- /*       grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                flipCount++;
-                imageAdapter.swapImageIds(position);
-                imageAdapter.notifyDataSetChanged();
-                flipCountText.setText("FLIP COUNT: " + flipCount);
-
-                //when all the 12 cards are matched, game is over
-                if (imageAdapter.getCardsMatched() == 12) {
-                    final Dialog dialog = new Dialog(context);
-                    // tell the Dialog to use the dialog.xml as it's layout
-                    // description
-                    dialog.setContentView(R.layout.dialog);
-                    dialog.setTitle(R.string.congratulations);
-
-                    TextView txt = (TextView) dialog.findViewById(R.id.txt);
-                    txt.setText("Your flipcount is: " + flipCount);
-                    Button newGameButton = (Button) dialog
-                            .findViewById(R.id.newGameButton);
-                    Button quitGameButton = (Button) dialog
-                            .findViewById(R.id.quitGameButton);
-
-                    newGameButton.setOnClickListener(new View.OnClickListener() {
-
-                        @Override
-                        public void onClick(View v) {
-                            dialog.dismiss();
-                            flipCount = 0;
-                            Intent i = new Intent(getApplicationContext(),
-                                    MainActivity.class);
-                            startActivity(i);
-                            finish();
-                        }
-                    });
-
-                    quitGameButton.setOnClickListener(new View.OnClickListener() {
-
-                        @Override
-                        public void onClick(View v) {
-                            flipCount = 0;
-                            finish();
-                        }
-                    });
-                    dialog.show();
-                }
+    private void createCardArrayList(ArrayList<BlankImageWithFaceImage> cardArrayList) {
+        for(int i =0; i<2 ; i++){
+            for(int j = 0; j<12; j++) {
+                cardArrayList.add(new BlankImageWithFaceImage(R.drawable.images, faceCardList[j]));
             }
-        });*/
+        }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         swapImageIds(position);
         if(cardsMatched == 12)
-        showGameOverDialog();
-    }
-
-    private void showGameOverDialog() {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-
-        alertDialogBuilder.setTitle(R.string.game_over_title);
-        alertDialogBuilder.setMessage(R.string.game_over_message);
-
-        // set positive button: New Game message
-        alertDialogBuilder.setPositiveButton(R.string.confirm_new_game_button,new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog,int id) {
-                /*isDateSet = true;
-
-                // go to a new activity of the app
-                SharedPreferences.Editor editor = mSharedPreferences.edit();
-                editor.putInt("Month", mDatePicker.getMonth());
-                editor.putInt("Day", mDatePicker.getDayOfMonth());
-                editor.putInt("Year", mDatePicker.getYear());
-                long dateTime = mDatePicker.getCalendarView().getDate();
-                Date date = new Date(dateTime);
-                editor.commit();*/
-                dialog.cancel();
-                Toast.makeText(getApplicationContext(), "You chose a positive answer",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-        // set negative button: Quit Game message
-        alertDialogBuilder.setNegativeButton(R.string.confirm_quit_game_button,new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog,int id) {
-                // cancel the alert box and put a Toast to the user
-                dialog.cancel();
-                Toast.makeText(getApplicationContext(), "You chose a negative answer",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        // show alert
-        alertDialog.show();
-
+            showGameOverDialog();
     }
 
     private void swapImageIds(int position) {
@@ -155,7 +76,7 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
             blankImageWithFaceImage.setBlankImageId(blankImageWithFaceImage.getFaceImageId());
             blankImageWithFaceImage.setFaceImageId(blankCardId);
             imageAdapter.notifyDataSetChanged();
-            flipCountEditText.setText("" +flipCount);
+            flipCountEditText.setText("" + flipCount);
             noOfCardsSelectedToCompare.add(blankImageWithFaceImage);
 
             if (noOfCardsSelectedToCompare.size() == 1) {
@@ -190,11 +111,41 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
         blankImageWithFaceImage.setFaceImageId(blankCardId);
     }
 
-    private void createCardArrayList(ArrayList<BlankImageWithFaceImage> cardArrayList) {
-        for(int i =0; i<2 ; i++){
-            for(int j = 0; j<12; j++) {
-                cardArrayList.add(new BlankImageWithFaceImage(R.drawable.images, faceCardList[j]));
+    private void showGameOverDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+
+        alertDialogBuilder.setTitle(R.string.game_over_title);
+        alertDialogBuilder.setMessage(getResources().getString(R.string.game_over_message) + "" + flipCount);
+
+        // set positive button: New Game message
+        alertDialogBuilder.setPositiveButton(R.string.confirm_new_game_button,new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int id) {
+                Intent i = new Intent(getApplicationContext(),
+                        MainActivity.class);
+                startActivity(i);
+                dismissAlertDialog(dialog);
             }
-        }
+        });
+        // set negative button: Quit Game message
+        alertDialogBuilder.setNegativeButton(R.string.confirm_quit_game_button,new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int id) {
+                // cancel the alert box and put a Toast to the user
+                dismissAlertDialog(dialog);
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show alert
+        alertDialog.show();
+    }
+
+    private void dismissAlertDialog(DialogInterface dialog) {
+        dialog.cancel();
+        flipCount = 0;
+        finish();
+    }
+
+    @Override
+    public Object onRetainCustomNonConfigurationInstance() {
+        return imageAdapter;
     }
 }
