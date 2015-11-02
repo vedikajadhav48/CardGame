19d04c63ab89448bf0e79431f1c0a2e135dc8bf2
Wedika Jadhav <vedikajadhav48@gmail.com>
2015-11-02 09:02:08 -0800

    Complete 2

--- app/src/main/java/com/example/vedikajadhav/cardgame/MainActivity.java
+++ app/src/main/java/com/example/vedikajadhav/cardgame/MainActivity.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
     private static int flipCount = 0;
+
     private ImageAdapter imageAdapter;
     private Integer[] faceCardList = {R.drawable.club2, R.drawable.club8, R.drawable.diamond6, R.drawable.diamond9, R.drawable.diamond_king,
             R.drawable.heart_king, R.drawable.heart_jack, R.drawable.heart_queen, R.drawable.heart3, R.drawable.spade4, R.drawable.spade_king,
